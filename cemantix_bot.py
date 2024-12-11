445208ae9dc7db1d1f388f975fdaef738f277488
Matis HERRMANN <matis_herrmann@hotmail.fr>
2024-12-11 19:21:27 +0100

    Fixed french and english versions

--- cemantix_bot.py
+++ cemantix_bot.py
@@ -1,3 +1,4 @@
+from functools import partial
 import json
 import os
 import random
@@ -7,11 +8,11 @@ import argparse
 from datetime import datetime, timedelta
 import shutil
 import requests
-from lxml import html
 import nltk
 from nltk.corpus import wordnet
 
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 red = "\033[1;31m"
 green = "\033[1;32m"
@@ -26,16 +27,33 @@ second = 1000
 percent = 100
 max_score = 1000.0
 
-website_url = 'https://cemantle.certitudes.org'
-headers = {"Origin": website_url, "Referer": website_url}
+languages = {
+    'en': {
+        'name': 'English',
+        'code': 'eng',
+        'url': 'https://cemantle.certitudes.org',
+    },
+    'fr': {
+        'name': 'French',
+        'code': 'fra',
+        'url': 'https://cemantix.certitudes.org',
+    },
+}
 
-dict_path = 'dic_eng.txt'
+dict_path = 'Dicts'
 days_path = "Days"
-today_file_path = f"{days_path}/{datetime.strftime(datetime.now(), '%d-%m-%Y')}.txt"
-yesterday_file_path = f"{days_path}/{datetime.strftime(datetime.now() - timedelta(days=1), '%d-%m-%Y')}.txt"
 
-if not os.path.exists('Days'):
-    os.makedirs('Days')
+today_file = f"{datetime.strftime(datetime.now(), '%d-%m-%Y')}.txt"
+yesterday_file = f"{datetime.strftime(datetime.now() - timedelta(days=1), '%d-%m-%Y')}.txt"
+
+
+for language in languages.keys():
+    if not os.path.exists(f"{days_path}/{language}"):
+        os.makedirs(f"{days_path}/{language}")
+
+    if not os.path.exists(f"{dict_path}/{language}.txt"):
+        os.makedirs(dict_path, exist_ok=True)
+        open(f"{dict_path}/{language}.txt", mode="w", encoding="utf-8").close()
 
 
 def get_max_value(dictionary):
@@ -48,7 +66,7 @@ def get_max_value(dictionary):
     return max_key, max_value
 
 
-def removeWordFromFile(file_path=dict_path, word='', words=[]):
+def removeWordFromFile(file_path, word='', words=[]):
     with open(file_path, mode="r", encoding="utf-8") as f:
         word_list = f.read().splitlines()
     if not word and not words:
@@ -59,27 +77,59 @@ def removeWordFromFile(file_path=dict_path, word='', words=[]):
                 f.write(w + "\n")
 
 
+def get_lexical_field(word, lang):
+    # Collect lexical field words
+    lexical_field = set()
+
+    try:
+        # Get synsets (sense groupings)
+        for synset in wordnet.synsets(word, lang=lang):
+            if not synset:
+                continue
+
+            # Add synonyms
+            for lemma in synset.lemmas():
+                lexical_field.add(lemma.name())
+            # Add hypernyms (broader terms)
+            for hypernym in synset.hypernyms():
+                lexical_field.update(lemma.name() for lemma in hypernym.lemmas())
+            # Add hyponyms (narrower terms)
+            for hyponym in synset.hyponyms():
+                lexical_field.update(lemma.name() for lemma in hyponym.lemmas())
+
+        lexical_field = [lf.replace('_', '-') for lf in list(lexical_field)]
+
+    except Exception as e:
+        with open('error.log', mode='a', encoding='utf-8') as f:
+            f.write(f'{word}: {e}\n')
+
+    return lexical_field
+
+
 def showProgress(count, total, width=25, symbol='-', name=''):
     line = "\r " + green + symbol * int(count / total * width) + red + symbol * (width - int(count / total * width)) + reset + f" {(count / total) * percent:.2f}% " + white + (f"[{name}]" if name else name) + reset
     print(line.ljust(shutil.get_terminal_size().columns), end="")
 
 
 def showRankings(ranking_size=25):
-    print(red + '''
-
-     ▄████████   ▄▄▄▄███▄▄▄▄      ▄████████ ███▄▄▄▄       ███      ▄█  ▀████    ▐████▀         ▄████████    ▄████████ ███▄▄▄▄      ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄     ▄████████ 
-    ███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███ ███▀▀▀██▄ ▀█████████▄ ███    ███▌   ████▀         ███    ███   ███    ███ ███▀▀▀██▄   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███   ███    ███ 
-    ███    █▀  ███   ███   ███   ███    ███ ███   ███    ▀███▀▀██ ███▌    ███  ▐███           ███    ███   ███    ███ ███   ███   ███▐██▀   ███▌ ███   ███   ███    █▀    ███    █▀  
-    ███        ███   ███   ███   ███    ███ ███   ███     ███   ▀ ███▌    ▀███▄███▀          ▄███▄▄▄▄██▀   ███    ███ ███   ███  ▄█████▀    ███▌ ███   ███  ▄███          ███        
-    ███        ███   ███   ███ ▀███████████ ███   ███     ███     ███▌    ████▀██▄          ▀▀███▀▀▀▀▀   ▀███████████ ███   ███ ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄  ▀███████████ 
-    ███    █▄  ███   ███   ███   ███    ███ ███   ███     ███     ███    ▐███  ▀███         ▀███████████   ███    ███ ███   ███   ███▐██▄   ███  ███   ███   ███    ███          ███ 
-    ███    ███ ███   ███   ███   ███    ███ ███   ███     ███     ███   ▄███     ███▄         ███    ███   ███    ███ ███   ███   ███ ▀███▄ ███  ███   ███   ███    ███    ▄█    ███ 
-    ████████▀   ▀█   ███   █▀    ███    █▀   ▀█   █▀     ▄████▀   █▀   ████       ███▄        ███    ███   ███    █▀   ▀█   █▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀   ▄████████▀  
-                                                                                              ███    ███                          ▀                                                  ''' + reset)
     ranking = {}
     for word, value in words_tested.items():
         ranking[word] = value
     ranking = sorted(ranking.items(), key=lambda x: x[1], reverse=True)
+
+    if len(ranking):
+        print(red + '''
+
+     ▄████████   ▄▄▄▄███▄▄▄▄      ▄████████ ███▄▄▄▄       ███      ▄█  ▀████    ▐████▀         ▄████████    ▄████████ ███▄▄▄▄      ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄     ▄████████
+    ███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███ ███▀▀▀██▄ ▀█████████▄ ███    ███▌   ████▀         ███    ███   ███    ███ ███▀▀▀██▄   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███   ███    ███
+    ███    █▀  ███   ███   ███   ███    ███ ███   ███    ▀███▀▀██ ███▌    ███  ▐███           ███    ███   ███    ███ ███   ███   ███▐██▀   ███▌ ███   ███   ███    █▀    ███    █▀
+    ███        ███   ███   ███   ███    ███ ███   ███     ███   ▀ ███▌    ▀███▄███▀          ▄███▄▄▄▄██▀   ███    ███ ███   ███  ▄█████▀    ███▌ ███   ███  ▄███          ███
+    ███        ███   ███   ███ ▀███████████ ███   ███     ███     ███▌    ████▀██▄          ▀▀███▀▀▀▀▀   ▀███████████ ███   ███ ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄  ▀███████████
+    ███    █▄  ███   ███   ███   ███    ███ ███   ███     ███     ███    ▐███  ▀███         ▀███████████   ███    ███ ███   ███   ███▐██▄   ███  ███   ███   ███    ███          ███
+    ███    ███ ███   ███   ███   ███    ███ ███   ███     ███     ███   ▄███     ███▄         ███    ███   ███    ███ ███   ███   ███ ▀███▄ ███  ███   ███   ███    ███    ▄█    ███
+    ████████▀   ▀█   ███   █▀    ███    █▀   ▀█   █▀     ▄████▀   █▀   ████       ███▄        ███    ███   ███    █▀   ▀█   █▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀   ▄████████▀
+                                                                                              ███    ███                          ▀                                                  ''' + reset)
+
     for i, (word, value) in enumerate(ranking):
         if i == ranking_size or value == 0:
             break
@@ -87,27 +137,26 @@ def showRankings(ranking_size=25):
               f"{(red if value > (max_score * 0.8) else yellow if value > (max_score * 0.5) else blue) + str(value) + reset}")
 
 
-def loadDict(file_path=today_file_path):
+def loadDict(file_path):
     if os.path.getsize(file_path) == 0:
         return {}
     with open(file_path, mode="r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def saveDict():
-    with open(today_file_path, mode="w", encoding="utf-8") as f:
+def saveDict(file_path):
+    with open(file_path, mode="w", encoding="utf-8") as f:
         json.dump(words_tested, f)
 
 
-def signal_handler(sig, frame):
-    saveDict()
-    removeWordFromFile(words=words_not_found)
+def signal_handler(sig, frame, path):
+    saveDict(path)
+    removeWordFromFile(file_path=path, words=words_not_found)
     showRankings()
     exit(0)
 
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, signal_handler)
     session = requests.Session()
     words_tested = {}
     words_to_test = []
@@ -116,16 +165,26 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Cemantix bot')
     parser.add_argument('-t', '--test', help='Test a word', type=str)
+    parser.add_argument('-l', '--language', help='Language', type=str, default='en')
     args = parser.parse_args()
 
-    if os.path.exists(dict_path):
-        with open(dict_path, mode="r", encoding="utf-8") as f:
+    website_url = languages[args.language]['url']
+    headers = {"Origin": website_url, "Referer": website_url}
+
+    today_file_path = f"{days_path}/{args.language}/{today_file}"
+    yesterday_file_path = f"{days_path}/{args.language}/{yesterday_file}"
+
+    global sigterm_handler
+    signal.signal(signal.SIGINT, partial(signal_handler, path=today_file_path))
+
+    if os.path.exists(f"{dict_path}/{args.language}.txt"):
+        with open(f"{dict_path}/{args.language}.txt", mode="r", encoding="utf-8") as f:
             for line in f.readlines():
                 words_to_test.append(line.replace("\n", ""))
         random.shuffle(words_to_test)
 
     if os.path.exists(today_file_path):
-        words_tested = loadDict()
+        words_tested = loadDict(today_file_path)
     else:
         open(today_file_path, mode="w", encoding="utf-8").close()
 
@@ -141,11 +200,6 @@ if __name__ == '__main__':
 
     while len(words_to_test) > 0 and max(words_tested.values()) < max_score if words_tested else True:
         word = words_to_test.pop()
-        
-        if word.endswith('s'):
-            words_to_test.append(word[:-1])
-        if word in words_tested:
-            continue
 
         res = session.post(website_url + "/score", headers=headers, data={"word": word})
         data = res.json() if res.content else {}
@@ -154,29 +208,7 @@ if __name__ == '__main__':
             last_result = {word: data['percentile']}
             words_tested[word] = float(data['percentile'])
 
-            try:
-                # Collect lexical field words
-                lexical_field = set()
-
-                # Get synsets (sense groupings)
-                for synset in wordnet.synsets(word, lang="eng"):
-                    # Add synonyms
-                    for lemma in synset.lemmas():
-                        lexical_field.add(lemma.name())
-                    # Add hypernyms (broader terms)
-                    for hypernym in synset.hypernyms():
-                        lexical_field.update(lemma.name() for lemma in hypernym.lemmas())
-                    # Add hyponyms (narrower terms)
-                    for hyponym in synset.hyponyms():
-                        lexical_field.update(lemma.name() for lemma in hyponym.lemmas())
-
-                lexical_field = [lf.replace('_', '-') for lf in list(lexical_field)]
-
-            except Exception as e:
-                with open('error.log', mode='a', encoding='utf-8') as f:
-                    f.write(f'{word}: {e}\n')
-
-            for lf in lexical_field:
+            for lf in get_lexical_field(word, languages[args.language]['code']):
                 if lf in words_tested:
                     continue
                 if lf in words_to_test:
@@ -202,4 +234,4 @@ if __name__ == '__main__':
                  name=f'{best_word}: {(green if best_value == max_score else red) + str(best_value) + white} | in {(green if tries < (max_score * 0.1) else yellow if tries < (max_score * 0.5) else red) + str(tries) + white} tries',
                  symbol='█')
 
-    signal_handler(None, None)
+    signal_handler(None, None, today_file_path)
